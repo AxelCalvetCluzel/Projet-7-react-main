b0b2e0b18866080f179861dda67258ce90d9db79
Calvet-Cluzel Axel <acalvet18@gmail.com>
2024-07-18 11:46:55 +0200

    mise a jour

--- Projet-7-react-main/projetReact/src/components/Collapse.jsx
+++ Projet-7-react-main/projetReact/src/components/Collapse.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import bannerAbout from "../images/banner2.png";
 
 const Collapse = ({ title, content }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -33,30 +32,4 @@ const Collapse = ({ title, content }) => {
   );
 };
 
-const Collapses = () => {
-  return (
-    <div className="containerCollapse">
-      <div className="bannerAbout">
-        <img className="img2" src={bannerAbout} alt="Seconde bannière" />
-      </div>
-      <Collapse
-        title="Fiabilité"
-        content="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."
-      />
-      <Collapse
-        title="Respect"
-        content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
-      />
-      <Collapse
-        title="Service"
-        content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
-      />
-      <Collapse
-        title="Sécurité"
-        content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
-      />
-    </div>
-  );
-};
-
-export default Collapses;
+export default Collapse;
